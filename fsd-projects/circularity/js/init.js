b5310ed621d5a4786352d779ff10e682b9fa3757
asonnierFHS <aubreesonnier@icloud.com>
2026-05-06 14:23:12 +0000

    completed circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -85,18 +85,18 @@ var init = function (window) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
             if ( circle.x > canvas.width ) {
-                circle.x = 0;
+                 circle.x = 0;
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
             else if(circle.x < 0) {
-                circle.x = canvas.width;
+             circle.x = canvas.width;
             }
             if(circle.y < 0) {
-                circle.y = canvas.height;
+             circle.y = canvas.height;
             }
             else if(circle.y > canvas.height) {
-                circle.y = 0;
+             circle.y = 0;
             }
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
